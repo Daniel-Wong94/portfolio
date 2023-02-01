42545077c311d039a01cd711e541bab47fb94b65
Daniel Wong <Daniel.kachun.wong@gmail.com>
2023-02-01 11:22:47 -0500

    use context for IO

--- src/App.js
+++ src/App.js
@@ -7,16 +7,39 @@ import {
   Project,
   Footer,
 } from "./components";
+import { IntersectionObserverContext } from "./context/IntersectionObserverContext";
+import styles from "./components/section/Section.module.css";
 
 const App = () => {
+  const options = {
+    root: null,
+    rootMargin: "10px",
+    threshold: 0.2,
+  };
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList = styles.fadeToLeft;
+        console.log("adding class");
+        observer.unobserve(entry.target);
+      }
+    });
+    return () => observer.disconnect();
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
   return (
     <BrowserRouter>
-      <NavBar />
-      <Main />
-      <Skills />
-      <Experience />
-      <Project />
-      <Footer />
+      <IntersectionObserverContext.Provider value={observer}>
+        <NavBar />
+        <Main />
+        <Skills />
+        <Experience />
+        <Project />
+        <Footer />
+      </IntersectionObserverContext.Provider>
     </BrowserRouter>
   );
 };
